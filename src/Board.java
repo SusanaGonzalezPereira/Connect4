c3cd073462bacd5af52e580fe423d4aad6ed3b4d
Diego Padilla <dpadilla@neuromobilemarketing.com>
2022-09-15 18:13:01 +0200

    Added Board Class Implementation.

--- src/Board.java
+++ src/Board.java
@@ -14,14 +14,38 @@ public class Board {
     }
 
     public void show() {
+        System.out.println("Tablero actual:");
         for ( int i = 0; i < MAX_ROWS ; i++ ){
             for ( int j = 0; j < MAX_COLUMNS; j++) {
-                System.out.println( "|" + map[i][j] );
+                System.out.print( "|" + map[i][j] );
             }
+            System.out.println("|");
         }
+        System.out.println("-------------------");
     }
 
     public boolean hasWinner() {
-        return false;
+        return isSolutionColumn() || isSolutionDiagonal() || isSolutionInverseDiagonal() || isSolutionRow();
+    }
+
+    private boolean isSolutionColumn(){
+        //TODO:Implementar
+        return true;
+
+    }
+
+    private boolean isSolutionRow() {
+        //TODO:Implementar
+        return true;
+    }
+
+    private boolean isSolutionDiagonal(){
+        //TODO:Implementar
+        return true;
+    }
+
+    private boolean isSolutionInverseDiagonal() {
+        //TODO:Implementar
+        return true;
     }
 }
